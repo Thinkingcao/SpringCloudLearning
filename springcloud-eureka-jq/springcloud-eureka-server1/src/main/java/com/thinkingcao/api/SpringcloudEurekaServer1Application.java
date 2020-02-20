e4bfcfa08c9091efd328db3bff2e8a7b677b9e46
Thinkingcao <617271837@qq.com>
2020-02-20 17:46:03 +0800

    SpringCloud系列教程(三)：SpringCloud之Eureka实现高可用集群环境搭建

---
+++ springcloud-eureka-jq/springcloud-eureka-server1/src/main/java/com/thinkingcao/api/SpringcloudEurekaServer1Application.java
@@ -0,0 +1,15 @@
+package com.thinkingcao.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringcloudEurekaServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEurekaServer1Application.class, args);
+    }
+
+}
