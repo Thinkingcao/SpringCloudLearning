c077f96fa31daa67af3666b5c6c6b08cc3b2410c
Thinkingcao <617271837@qq.com>
2020-02-20 15:49:10 +0800

    SpringCloud系列教程(二)：搭建服务提供者和服务消费者之Rest+Ribbon方式

--- springcloud-ribbon/springcloud-app-member/src/main/java/com/thinkingcao/api/controller/MemberController.java
+++ springcloud-ribbon/springcloud-app-member/src/main/java/com/thinkingcao/api/controller/MemberController.java
@@ -1,18 +1,23 @@
 package com.thinkingcao.api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @desc:
+ * @desc:   会员服务接口
  * @author: cao_wencao
  * @date: 2020-02-19 18:12
  */
 @RestController
 public class MemberController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
+    //会员服务接口
     @RequestMapping("/getMember")
     private String getMember() {
-        return "我是会员服务，订单服务调用会员服务成功啦";
+        return "我是会员服务，订单服务调用会员服务成功啦, 端口号为： " + serverPort;
     }
 }
--- springcloud-ribbon/springcloud-app-order/src/main/java/com/thinkingcao/api/controller/OrderController.java
+++ springcloud-ribbon/springcloud-app-order/src/main/java/com/thinkingcao/api/controller/OrderController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * @desc:
+ * @desc:  订单调用会员服务接口
  * @author: cao_wencao
  * @date: 2020-02-19 17:47
  */
@@ -26,6 +26,8 @@ public class OrderController {
         return result;
     }
 
+    // 有两种方式调用，一种是采用服务别名方式调用，使用别名方式会去注册中心上获取对应的服务调用地址；
+    // 另一种是直接使用IP地址调用(不会走注册中心)
     @Bean
     @LoadBalanced
     RestTemplate restTemplate() {
