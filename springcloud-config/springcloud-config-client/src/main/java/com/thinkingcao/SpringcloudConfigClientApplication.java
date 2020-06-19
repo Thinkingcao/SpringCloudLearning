dc19fce974950f847ac3119ad3dcb8cb227e4ad9
Thinkingcao <617271837@qq.com>
2020-06-19 10:29:07 +0800

    Spring Cloud系列教程(十)：分布式配置中心(Spring Cloud Config)

---
+++ springcloud-config/springcloud-config-client/src/main/java/com/thinkingcao/SpringcloudConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.thinkingcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigClientApplication.class, args);
+    }
+
+}
