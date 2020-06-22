ab23d72269f65e0eecba75ba3a39ccbe282a37fe
Thinkingcao <617271837@qq.com>
2020-06-22 18:16:51 +0800

    Spring Cloud系列教程(十一)：消息总线Spring Cloud Bus(Finchley版本)

---
+++ springcloud-bus/springcloud-config-client/src/main/java/com/thinkingcao/AppConfigClient.java
@@ -0,0 +1,13 @@
+package com.thinkingcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfigClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfigClient.class, args);
+    }
+
+}
