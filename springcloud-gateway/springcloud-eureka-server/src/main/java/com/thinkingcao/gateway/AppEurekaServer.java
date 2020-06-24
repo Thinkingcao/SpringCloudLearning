e473860de74866cda709282a6336c35811fd1dd3
Thinkingcao <617271837@qq.com>
2020-06-24 16:37:00 +0800

    Spring Cloud系列教程(十六)：下一代网关服务Gateway(Finchley版本)

---
+++ springcloud-gateway/springcloud-eureka-server/src/main/java/com/thinkingcao/gateway/AppEurekaServer.java
@@ -0,0 +1,15 @@
+package com.thinkingcao.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AppEurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppEurekaServer.class, args);
+    }
+
+}
